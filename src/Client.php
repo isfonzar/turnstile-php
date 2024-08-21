84a26156366e1f40e9c71e51c9c9324a4435f086
isfonzar <icaro.isf@gmail.com>
2024-08-21 12:13:02 -0300

    Add Guzzle for HTTP calls to challenge endpoint

--- src/Client.php
+++ src/Client.php
@@ -1,16 +1,39 @@
 <?php
 
 namespace Isfonzar\TurnstilePhp;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\GuzzleException;
+
 class Client
 {
     const CHALLENGE_URL = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
 
+    private GuzzleClient $http;
+
     public function __construct(
         private string $secret,
-    ) { }
+        ?GuzzleClient $http = null,
+    ) {
+        $this->http = $http ?? new GuzzleClient();
+    }
 
     public function verify(string $response): bool
     {
-        return true;
+        try {
+            $result = $this->http->request('POST', self::CHALLENGE_URL, [
+                'form_params' => [
+                    'secret' => $this->secret,
+                    'response' => $response
+                ]
+            ]);
+
+            $body = $result->getBody();
+            $decodedResult = json_decode($body, true);
+
+            return $decodedResult['success'] ?? false;
+        } catch (GuzzleException $e) {
+            throw new \RuntimeException("Failed to send request: " . $e->getMessage());
+        }
     }
 }
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,20 +1,43 @@
 <?php
 
-namespace Isfonzar\tests;
+namespace Isfonzar\Tests;
 
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Psr7\Response;
 use Isfonzar\TurnstilePhp\Client;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class ClientTest extends TestCase
 {
-    public function testVerify()
+    private GuzzleClient $guzzleMock;
+    private Client $client;
+
+    protected function setUp(): void
     {
-        $mockKey = 's3cr3tk3y';
-        $client = new Client($mockKey);
+        $this->guzzleMock = $this->createMock(GuzzleClient::class);
+        $this->client = new Client('s3cr3tk3y', $this->guzzleMock);
+    }
+
+    #[DataProvider('responseProvider')]
+    public function testVerify(bool $expectedSuccess, bool $apiResponse)
+    {
+        $responseMock = new Response(200, [], json_encode(['success' => $apiResponse]));
+
+        $this->guzzleMock->method('request')
+            ->willReturn($responseMock);
 
         $input = "cf-turnstile-response";
-        $response = $client->verify($input);
+        $response = $this->client->verify($input);
 
-        $this->assertTrue($response);
+        $this->assertEquals($expectedSuccess, $response);
+    }
+
+    public static function responseProvider(): array
+    {
+        return [
+            [true, true],  // Test case where API returns success true
+            [false, false] // Test case where API returns success false
+        ];
     }
 }
